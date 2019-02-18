e693d03eb69f4f3674960bbab9e295e98bf46b4a
Remicy <remicyow@gmail.com>
2019-02-19 06:21:10 +0800

    V2

---
+++ precache-manifest.95ae40ffe6c1d46b740307da81edf804.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "acfa1f73b7e2fb0e0594",
+    "url": "./static/js/main.acfa1f73.chunk.js"
+  },
+  {
+    "revision": "8a212ff0c4aa4487f555",
+    "url": "./static/js/2.8a212ff0.chunk.js"
+  },
+  {
+    "revision": "acfa1f73b7e2fb0e0594",
+    "url": "./static/css/main.97fa6b23.chunk.css"
+  },
+  {
+    "revision": "22792eaae3c0795f189f40540bef4e9e",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e59b6f6c7dd6154aaba971c36f915af8.js"
+  "./precache-manifest.95ae40ffe6c1d46b740307da81edf804.js"
 );
 
 workbox.clientsClaim();
